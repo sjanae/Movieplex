16bb437ba630ce6bd0eed64bdb3f11a22a958cfd
Shatavia <shataviajv@live.com>
2015-11-28 19:17:33 -0600

    add show route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
 	def index
-		@movies = Movie.all
+		@movie = Movie.all
+	end
+
+	def show
+		@movie = Movie.find(params[:id])
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
- get "movies" => "movies#index"
+root "movies#index"
+ get "movies" 		=> "movies#index"
+ get "movies/:id" 	=> "movies#show", as: "movie"
 end
